cd35e944c82918274ef214adb8fc74fbe70cdff3
And4Web <anands2807@gmail.com>
2022-01-21 08:16:35 -0800

    layout created.

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,42 @@
 import React from "react";
 
 export default function Main() {
-  return <h1>Main Component...</h1>;
+  return (
+    <div className="row justify-content-center">
+      <div className="col-md-5">
+        <h1>React Calculator</h1>
+        <table className="table">
+          <tr>
+            <input type="text"></input>
+            <button>C</button>
+          </tr>
+
+          <tr>
+            <button>1</button>
+            <button>2</button>
+            <button>3</button>
+            <button>/</button>
+          </tr>
+          <tr>
+            <button>4</button>
+            <button>5</button>
+            <button>6</button>
+            <button>+</button>
+          </tr>
+          <tr>
+            <button>7</button>
+            <button>8</button>
+            <button>9</button>
+            <button>-</button>
+          </tr>
+          <tr>
+            <button>.</button>
+            <button>0</button>
+            <button>=</button>
+            <button>*</button>
+          </tr>
+        </table>
+      </div>
+    </div>
+  );
 }
